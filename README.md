26a5c4d99612a6dbcfb6b563c582ed5b81813edf
Shuyuan <crayy.0803@gmail.com>
2023-05-11 02:19:52 +0800

    First commit

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Getting Started with Create React App
+# Pokedex App
+
+This is a Pokedex application built with React and Bootstrap. It uses the [PokeAPI](https://pokeapi.co/) to fetch and display Pokemon details. The API is free to use and does not require authentication.
+
+## Features
+- List display of Pokemon data
+- Search functionality to find Pokemon by name
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
@@ -42,29 +50,3 @@ You don't have to ever use `eject`. The curated feature set is suitable for smal
 ## Learn More
 
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [searchedPokemon, setSearchedPokemon] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [message, setMessage] = useState('');
+
+  const colours = {
+    normal: '#A8A77A',
+    fire: '#EE8130',
+    water: '#6390F0',
+    electric: '#F7D02C',
+    grass: '#7AC74C',
+    ice: '#96D9D6',
+    fighting: '#C22E28',
+    poison: '#A33EA1',
+    ground: '#E2BF65',
+    flying: '#A98FF3',
+    psychic: '#F95587',
+    bug: '#A6B91A',
+    rock: '#B6A136',
+    ghost: '#735797',
+    dragon: '#6F35FC',
+    dark: '#705746',
+    steel: '#B7B7CE',
+    fairy: '#D685AD',
+  };
+
+  const fetchPokemonList = async () => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon?limit=50`);
+      const promises = response.data.results.map(result => axios.get(result.url));
+      const pokemonResponses = await Promise.all(promises);
+      const pokemons = pokemonResponses.map(res => res.data);
+      setPokemonList(pokemons);
+      setMessage('');
+    } catch (error) {
+      setPokemonList([]);
+      setMessage('Failed to fetch Pokemon');
+    }
+  };
+
+  const fetchPokemon = async (name = '') => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
+      setSearchedPokemon([response.data]);
+      setMessage('');
+    } catch (error) {
+      setSearchedPokemon(null);
+      setMessage('No Pokemon found with that name');
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemonList();
+  }, []);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    if (searchTerm !== '') {
+      fetchPokemon(searchTerm);
+    } else {
+      setSearchedPokemon(null);
+    }
+  };
+
+  // Helper function to capitalize first letter of strings
+  const capitalizeFirstLetter = string => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <form onSubmit={handleSearch}>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search for a Pokemon..."
+            onChange={event => setSearchTerm(event.target.value)}
+          />
+          <button className="btn btn-primary" type="submit">
+            Search
+          </button>
+        </div>
+      </form>
+
+      {message && <div className="alert alert-warning mt-3">{message}</div>}
+      <table className="table mt-3">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Height</th>
+            <th scope="col">Weight</th>
+            <th scope="col">Type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {!searchedPokemon && pokemonList.map(pokemon => (
+            <tr key={pokemon.id} className='align-middle'>
+              <td>{pokemon.id}</td>
+              <td>
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} style={{ width: '100px' }} />
+              </td>
+              <td>{capitalizeFirstLetter(pokemon.name)}</td>
+              <td>{pokemon.height}</td>
+              <td>{pokemon.weight}</td>
+              <td>
+                {pokemon.types.map((type, index) => (
+                  <span key={index} style={{
+                    backgroundColor: colours[type.type.name],
+                    color: '#fff',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                    marginRight: '5px',
+                  }}>
+                    {capitalizeFirstLetter(type.type.name)}
+                  </span>
+                ))}
+              </td>
+            </tr>
+          ))}
+          {searchedPokemon && searchedPokemon.map(pokemon => (
+            <tr key={pokemon.id} className='align-middle'>
+              <td>{pokemon.id}</td>
+              <td>
+                <img src={pokemon.sprites.front_default} alt={pokemon.name} style={{ width: '100px' }} />
+              </td>
+              <td>{capitalizeFirstLetter(pokemon.name)}</td>
+              <td>{pokemon.height}</td>
+              <td>{pokemon.weight}</td>
+              <td>
+                {pokemon.types.map((type, index) => (
+                  <span key={index} style={{
+                    backgroundColor: colours[type.type.name],
+                    color: '#fff',
+                    padding: '5px 10px',
+                    borderRadius: '5px',
+                    marginRight: '5px',
+                  }}>
+                    {capitalizeFirstLetter(type.type.name)}
+                  </span>
+                ))}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
